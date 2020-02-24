60c57b7ddd3ea9bed3497a2354ea906f3131354d
Ukeje Michgolden <michgoldennathan17@gmail.com>
2020-02-24 17:47:04 +0100

    [Feature] Add dark mode switch on page

--- src/script.js
+++ src/script.js
@@ -1,4 +1,22 @@
-    document.querySelector('.ball').addEventListener('animationend', function (e) {
-      e.target.firstChild.innerText = "GitHub";
-      e.target.classList.replace("ball", "expand");
-    }, false);
+document.querySelector(".ball").addEventListener(
+  "animationend",
+  function(e) {
+    e.target.firstChild.innerText = "GitHub";
+    e.target.classList.replace("ball", "expand");
+  },
+  false
+);
+
+const toggleSwitch = document.querySelector(
+  'input[type="checkbox"]'
+);
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark-theme");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light-theme");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
