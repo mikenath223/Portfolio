aacd458d7cfd575cd1d9d0fc36a55bca37571bfe
mikenath223 <michgoldennathan17@gmail.com>
2020-05-29 21:41:46 +0100

    [Update] Add social links at top bar

--- src/script.js
+++ src/script.js
@@ -1,7 +1,15 @@
 document.querySelector(".ball").addEventListener(
   "animationend",
-  function(e) {
-    e.target.firstChild.innerText = "GitHub";
+  (e) => {
+    e.target.firstChild.innerHTML = `<a href="https://github.com/mikenath223" class="soc-ball" rel="noopener" target="_blank">
+    <img
+    src="./src/assets/icons8-github-30.png" class="git-soc" alt="github""></a>
+    <a href="https://github.com/mikenath223" class="soc-ball" rel="noopener" target="_blank">
+    <img
+    src="./src/assets/icons8-linkedin-30.png" alt="github"> </a>
+    <a href="https://github.com/mikenath223" class="soc-ball git-soc" rel="noopener" target="_blank">
+    <img
+    src="./src/assets/icons8-twitter-30.png" alt="github"> </a>`;
     e.target.classList.replace("ball", "expand");
   },
   false
@@ -11,11 +19,14 @@ const toggleSwitch = document.querySelector(
   'input[type="checkbox"]'
 );
 
-function switchTheme(e) {
+const switchTheme = (e) => {
+  const gitSoc = document.querySelector('.git-soc');
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark-theme");
+    gitSoc.src = "./src/assets/icons8-github-white.png";
   } else {
     document.documentElement.setAttribute("data-theme", "light-theme");
+    gitSoc.src = "./src/assets/icons8-linkedin-30.png";
   }
 }
 
