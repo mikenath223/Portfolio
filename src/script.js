b8636645359f3dc6ef4ab6adb2dbb40fccde1136
Michgolden Ukeje <mich.u@bettercast.io>
2023-07-13 16:02:10 +0100

    Update project list and fix animation toggle

--- src/script.js
+++ src/script.js
@@ -1,7 +1,25 @@
-document.querySelector(".ball").addEventListener(
-  "animationend",
-  (e) => {
-    e.target.firstChild.innerHTML = `<a href="https://github.com/mikenath223" class="soc-ball" rel="noopener" target="_blank">
+const onDarkMode = (cb) => {
+  const isDarkMode = window?.matchMedia(
+    "(prefers-color-scheme: dark)"
+  )?.matches;
+
+  if (isDarkMode) {
+    document.documentElement.setAttribute("data-theme", "dark-theme");
+    cb?.();
+  }
+};
+
+const onToggleGitSrc = () => {
+  const gitSoc = document.querySelector(".git-soc");
+  gitSoc.src = "./src/assets/icons8-github-white.png";
+};
+
+onDarkMode();
+
+document.querySelector(".ball").addEventListener("animationend", (e) => {
+  e.target.querySelector(
+    "p"
+  ).innerHTML = `<a href="https://github.com/mikenath223" class="soc-ball" rel="noopener" target="_blank">
     <img
     src="./src/assets/icons8-github-30.png" class="git-soc" alt="github""></a>
     <a href="https://www.linkedin.com/in/michgoldenukeje/" class="soc-ball" rel="noopener" target="_blank">
@@ -10,15 +28,14 @@ document.querySelector(".ball").addEventListener(
     <a href="https://twitter.com/MichgoldenU" class="soc-ball git-soc" rel="noopener" target="_blank">
     <img
     src="./src/assets/icons8-twitter-30.png" alt="github"> </a>`;
-    e.target.classList.replace("ball", "expand");
-  });
+  e.target.classList.replace("ball", "expand");
+  onDarkMode(onToggleGitSrc);
+});
 
-const toggleSwitch = document.querySelector(
-  'input[type="checkbox"]'
-);
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
 const switchTheme = (e) => {
-  const gitSoc = document.querySelector('.git-soc');
+  const gitSoc = document.querySelector(".git-soc");
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark-theme");
     gitSoc.src = "./src/assets/icons8-github-white.png";
@@ -26,6 +43,6 @@ const switchTheme = (e) => {
     document.documentElement.setAttribute("data-theme", "light-theme");
     gitSoc.src = "./src/assets/icons8-github-30.png";
   }
-}
+};
 
 toggleSwitch.addEventListener("change", switchTheme);
