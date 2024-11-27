4d4bced1ad246bb5e51a605da33b39d7ad8ee462
Michgolden Ukeje <michgoldennathan17@gmail.com>
2024-11-27 14:05:50 +0100

    Update projects list

--- src/constants/projects.constants.ts
+++ src/constants/projects.constants.ts
@@ -2,14 +2,6 @@ import { Project } from "@src/interfaces/project.interface";
 
 export const projects: Project[] = [
   {
-    title: "Bettercast Event Hosting",
-    description:
-      "Build the MVP product, and successfully onboarded the first set of users handling 2000+ capacity events at the first launch.",
-    image: "/images/bettercast-mock.png",
-    technologies: ["ReScript", "React", "TypeScript", "CSS", "JavaScript"],
-    link: "https://conference.bettercast.io",
-  },
-  {
     title: "Peoplebox - OKR and Strategy Execution Platform for Enterprises",
     description:
       "Built the system to create and track OKRs that increased user signups by 200%. Updated the existing flow for objectives and goals analysis that reduced churn rate by 40%.",
@@ -33,61 +25,12 @@ export const projects: Project[] = [
     link: "https://www.joinexec.com/",
   },
   {
-    title: "Facebook Clone",
+    title: "Bettercast Event Hosting",
     description:
-      "A clone of the Facebook social media platform. It implements core features of Facebook such as friending, posting, commenting and liking.",
-    image: "/images/facebrook-mockshot.png",
-    technologies: ["Ruby on Rails", "HTML", "CSS", "JavaScript"],
-    sourceLink: "https://github.com/mikenath223/Project-Building-Facebook",
-  },
-  {
-    title: "Trixxcare",
-    description:
-      "Built a health care delivery app built with the React and Redux frameworks coupled with a backend API built with Ruby on Rails. It showcases various caregivers and gives the opportunity for patients to book appoitments with a caregiver.",
-    image: "/images/trixx.png",
-    technologies: [
-      "Rails",
-      "Ruby",
-      "React",
-      "Redux",
-      "React-Router",
-      "HTML5-CSS3",
-      "JavaScript",
-    ],
-    // link: "https://trixxcare.netlify.app/",
-    frontendSource: "https://github.com/mikenath223/trixxcare",
-    backendSource: "https://github.com/mikenath223/trixxcare-api",
-  },
-  {
-    title: "MunchIt",
-    description:
-      "Created a meals recipe app built with the React and Redux frameworks. It showcases meals accoring to their categories and each meal contains instructions on how to prepare also included is a Youtube video.",
-    image: "/images/munch.png",
-    technologies: [
-      "React",
-      "Redux",
-      "React-Router",
-      "CSS-Modules",
-      "JavaScript",
-    ],
-    // link: "https://muunchit.herokuapp.com/",
-    sourceLink: "https://github.com/mikenath223/Cook-book-app",
-  },
-  {
-    title: "Project Weather App",
-    description:
-      "Built a weather forecast app built with the openweather API with webpack. It also implements the GoogleMaps API to showcase locations.",
-    image: "/images/weather-mock.png",
-    technologies: [
-      "OpenWeatherAPI",
-      "GoogleMapsAPI",
-      "GoogleTimeZoneAPI",
-      "HTML",
-      "CSS",
-      "JavaScript",
-    ],
-    link: "https://mikenath223.github.io/Project-Weather-App/",
-    sourceLink: "https://github.com/mikenath223/Project-Weather-App",
+      "Build the MVP product, and successfully onboarded the first set of users handling 2000+ capacity events at the first launch.",
+    image: "/images/bettercast-mock.png",
+    technologies: ["ReScript", "React", "TypeScript", "CSS", "JavaScript"],
+    link: "https://conference.bettercast.io",
   },
   {
     title: "This Portfolio",
